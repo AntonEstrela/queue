201e7330252c75dfb10c3548bbb0c09bdc704365
AntonEstrela <anton.estrela@gmail.com>
2019-03-06 22:05:20 +0100

    Ya funciona bien, acabado

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,27 +6,25 @@ public class Main {
 
     public static void main(String[] args) {
         Random ran = new Random();
-        Queue<int> q = new Queue<int>(10);
+        Queue<Integer> q = new QueueImpl<Integer>(10);
         for (int i = 0; i < 11; i++){
             int tmp = ran.nextInt(100);
-            System.out.print("Size: " + q.size() + "\tPush: " + tmp);
+            System.out.print("isFull: " + q.isFull() + " isEmpty: " + q.isEmpty() + " Size: " + q.size() + " Push: " + tmp);
             try {
                 q.push(tmp);
+                System.out.println(" -> OK");
             }
             catch(CuaPlenaException e){
                 System.out.println(" -> CuaPlenaException, amb size: " + e.GetSize());
             }
-            finally{
-                System.out.println(" -> OK");
-            }
         }
         for (int i = 0; i < 11; i++){
-            System.out.print("Size: " + q.size());
+            System.out.print("isFull: " + q.isFull() + " isEmpty: " + q.isEmpty() + " Size: " + q.size());
             try {
-                System.out.println("Pop: " + q.pop());
+                System.out.println(" Pop: " + q.pop());
             }
             catch(CuaBuidaException e){
-                System.out.println(" -> CuaBuidaException");
+                System.out.println(" Pop -> CuaBuidaException");
             }
         }
     }
