05c7491b201e5935495336cd92307a46ef779030
AntonEstrela <anton.estrela@gmail.com>
2019-03-06 21:50:20 +0100

    main, con errores

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,33 @@
 package com.company;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+        Random ran = new Random();
+        Queue<int> q = new Queue<int>(10);
+        for (int i = 0; i < 11; i++){
+            int tmp = ran.nextInt(100);
+            System.out.print("Size: " + q.size() + "\tPush: " + tmp);
+            try {
+                q.push(tmp);
+            }
+            catch(CuaPlenaException e){
+                System.out.println(" -> CuaPlenaException, amb size: " + e.GetSize());
+            }
+            finally{
+                System.out.println(" -> OK");
+            }
+        }
+        for (int i = 0; i < 11; i++){
+            System.out.print("Size: " + q.size());
+            try {
+                System.out.println("Pop: " + q.pop());
+            }
+            catch(CuaBuidaException e){
+                System.out.println(" -> CuaBuidaException");
+            }
+        }
     }
 }
